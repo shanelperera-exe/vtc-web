c23236490c5a6df4b2f114ed52c61d8ae9a0087a
Abeyweera A.M.P.V.T.C <IT24103428@my.sliit.lk>
2025-10-12 11:14:17 +0530

    Added the relevant dto classes for authentication

---
+++ backend/src/main/java/com/vtcweb/backend/dto/user/UserResponse.java
@@ -0,0 +1,19 @@
+package com.vtcweb.backend.dto.user;
+
+import com.vtcweb.backend.model.entity.user.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private UserRole role;
+}
